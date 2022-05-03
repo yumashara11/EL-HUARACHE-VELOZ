63bd227774b803ff61bff0a134b67790adb08ff3
yumashara11 <78113616+yumashara11@users.noreply.github.com>
2022-05-02 21:53:50 -0500

    inventario.h
    archivo .h del proyecto

---
+++ inventario.h
@@ -0,0 +1,28 @@
+#ifndef INVENTARIO_H_INCLUDED
+#define INVENTARIO_H_INCLUDED
+typedef struct inventario{
+    char nombre[30];
+    float precio;
+    int cantidad;
+    struct inventario *sig;
+    struct inventario *ant;
+}inventario;
+
+typedef struct lista{
+    struct inventario *inicio;
+    struct inventario *fin;
+    int n;
+}lista;
+
+lista *nuevaLista();
+
+inventario*crear(char nombre2[],int n,float precio,int cantidad);
+
+void agregaDatoFinal(lista *listado,char nombre[],int n,float precio,int cantidad);
+
+int vacia(lista *ListaF);
+void imprimeLista(lista *ListaF);
+void llenarInventario(char nombre[],int n,float*precio,int*cantidad,lista*ListaF);
+
+
+#endif // INVENTARIO_H_INCLUDED
