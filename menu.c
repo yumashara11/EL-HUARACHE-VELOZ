a338e839435a8b21d53e76386ba4d87bb7c8a9ba
yumashara11 <78113616+yumashara11@users.noreply.github.com>
2022-05-04 07:34:13 -0500

    Create menu.c

---
+++ menu.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void menu(int *op){
+    printf("*******EL HUARACHE VELOZ*******\n");
+    printf("**TIENDA ONLINE 100% MEXICANA**\n");
+    printf("IDENTIFICATE\n");
+    printf("[1]ALMACENISTA\n[2]CLIENTE\n[3]GERENTE\n[4]REPARTIDOR\n[5]SALIR\n");
+    scanf("%d",op);
+}
