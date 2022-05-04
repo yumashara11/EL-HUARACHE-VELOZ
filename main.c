b3ee3bffcf1b6fb9d44a1491411a0e49b6afa826
yumashara11 <78113616+yumashara11@users.noreply.github.com>
2022-05-03 20:26:41 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include"inventario.h"
-
+#include "menu.h"
 int main()
 {
     char nombre[30];
     float precio;
     int cantidad;
+    int opcion;
+
     lista *catalogo=nuevaLista();
-    llenarInventario(nombre,30,&precio,&cantidad,catalogo);
-    imprimeLista(catalogo);
+    lista *carrito=nuevaLista();
+    do {
+    menu(&opcion);
+    switch(opcion)
+    {
+    case 1:
+        llenarInventario(nombre,30,&precio,&cantidad,catalogo);
+        break;
+    case 2:
+         llenarCarrito(catalogo);
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+
+    }
+    }while(opcion!=5);
+
     return 0;
 }
-
