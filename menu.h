22eb82c8bdaca3654a05c5f9aa45ab46bf57eda2
yumashara11 <78113616+yumashara11@users.noreply.github.com>
2022-05-05 23:37:12 -0500

    Update menu.h

--- menu.h
+++ menu.h
@@ -2,5 +2,8 @@
 #define MENU_H_INCLUDED
 
 void menu(int*op);
-
+void menu_cliente(int *opcion);
+void menu_almacenista(int *opcion);
+void preguntas_usuario(int*op,int n);
+void navegacion(char *caracter);
 #endif // MENU_H_INCLUDED
