389c3effa68e92ee166f6c2c9a7d17d89deff088
yumashara11 <78113616+yumashara11@users.noreply.github.com>
2022-05-05 23:37:35 -0500

    Update menu.c

--- menu.c
+++ menu.c
@@ -7,3 +7,52 @@ void menu(int *op){
     printf("[1]ALMACENISTA\n[2]CLIENTE\n[3]GERENTE\n[4]REPARTIDOR\n[5]SALIR\n");
     scanf("%d",op);
 }
+void menu_cliente(int *opcion)
+{
+    printf("*****CLIENTE*****\n");
+    printf("[1]LLENAR CARRITO\n[2]EDITAR CARRITO\n[3]CONSULTAR CARRITO COMPLETO\n");
+    printf("[4]SALIR\n");
+    scanf("%d",opcion);
+}
+void menu_almacenista(int *opcion)
+{
+    printf("*****ALMACENISTA*****\n");
+    printf("[1]CONSULTAR EL INVENTARIO\n[2]MODIFICAR INVENTARIO\n[3]SALIR\n");
+    scanf("%d",opcion);
+}
+void preguntas_usuario(int*op,int n)
+{
+    switch(n)
+    {
+        case 1:
+            printf("desea agregar el producto al carrito? 1[SI] - 0[NO]: ");
+            scanf("%d",op);
+            return;
+        case 2:
+            printf("\ncuantos productos desea agregar?: ");
+            scanf("%d",op);
+            return;
+        case 3:
+            printf("FINALIZAR COMRA [1] - SEGUIR COMPRANDO [0]: ");
+            scanf("%d",op);
+            return;
+        case 4:
+            printf("deseas eliminar este producto? 1[SI] - 0[NO]: ");
+            scanf("%d",op);
+            return;
+        case 5:
+            printf("cuantos productos desea eliminar?: ");
+            scanf("%d",op);
+            return;
+        case 6:
+             printf("FINALIZAR PEDIDO[1] - SEGUIR EDITANDO[0]: ");
+            scanf("%d",op);
+            return;
+    }
+    return;
+}
+void navegacion(char *caracter)
+{
+    printf("SIGUIENTE [s] - ANTERIOR [a] - FINAL[f] - INICIO [i]: ");
+    scanf("%c",caracter);
+}
