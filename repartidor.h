35fd319a8221591506e7f02ba77f07e710259466
yumashara11 <78113616+yumashara11@users.noreply.github.com>
2022-05-13 09:40:51 -0500

    Create repartidor.h

---
+++ repartidor.h
@@ -0,0 +1,23 @@
+#ifndef REPARTIDOR_H_INCLUDED
+#define REPARTIDOR_H_INCLUDED
+#include "gerente.h"
+typedef struct repartidor{
+    char nrepartidor[20];
+    double id;
+    struct repartidor *sig;
+}repartidor;
+
+typedef struct colaRepartidores{
+    struct repartidor*inicio;
+    struct repartidor*fin;
+    int n;
+}colaRepartidores;
+int colaVacia_R(colaRepartidores*repartidor);
+colaRepartidores *nuevaColaR(void);
+repartidor*crear_3(char nrepartidor[],int n,double id);
+void agregaDatoCola_R(char nrepartidor[],int n,double id,colaRepartidores*disponibles);
+void llenarRepartidores(char nrepartidor[],int n,double *id,colaRepartidores*repartidores);
+void imprimirCola_R(colaRepartidores*cola);
+
+
+#endif // REPARTIDOR_H_INCLUDED
